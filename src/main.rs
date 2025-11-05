98b67eb252fa9f8710e6847ec389ff39a2dd47f8
k0zihyp3r <kacperkozlow10@gmail.com>
2025-11-05 23:53:03 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,6 @@
+let url = format!( "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric" );
+let resp = reqwest::get(&url).await?.json::<serde_json::Value>().await?;
+
+
 fn main() {
 }
