8a1a7eb116c3f93e30ff8edc607dd6445a16cd55
k0zihyp3r <kacperkozlow10@gmail.com>
2025-11-08 01:22:11 +0100

    basic logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
-let url = format!( "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric" );
-let resp = reqwest::get(&url).await?.json::<serde_json::Value>().await?;
+use std::io;
+use serde::Deserialize;
+use colored::*;
 
+#[dervie(Deserialize, Debug)]
+struct WeatherResponse{
+    weather: Vec<Weather>,
+    main: Main,
+    wind: Wind,
+    name: String,
+}
 
+#[dervie(Deserialize, Debug)]
+struct WeatherDesc{
+    description: String,
+
+}
+
+#[dervie(Deserialize, Debug)]
+struct WeatherParameters{
+    temp: f64,
+    pressure: f64,
+    humidity: f64,
+}
+
+#[dervie(Deserialize, Debug)]
+struct Wind{
+    speed:f64,
+}
+
+fn get_weather(city: &str, api_key: &str) ->
+    Result<WeatherRespone, reqwest::Error>{
+    let url: string = format!( "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric", city, api);
+    let resp = reqwest::blocking::get(&url)?;
+    let respone_json: WeatherResponse = Response.json::<WeatherResponse>()?;
+    Ok(respone_json);
+
+}
 fn main() {
+
+    
+    
 }
